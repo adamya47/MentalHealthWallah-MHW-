c58cbd48009f4a9a02c010ee93754feeb068e00c
adamya47 <adamya47@gmail.com>
2024-09-04 14:33:50 -1200

    "reseted the vercel.js +cors options"

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,14 +8,11 @@ export const app=express()
 app.use(cookieParser())
 const corsOptions={
   origin:['https://mental-health-wallah-mhw.vercel.app'],
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-  credentials:true
+    credentials:true
   }
 
 app.use(cors(corsOptions))
 
-
-app.options('*', cors(corsOptions));
 
 app.use(express.json({
     limit:"16kb"
